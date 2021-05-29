b5f9e43dca481d8d6547ff6ed04178422518aea5
Kirill <kirillesnikh@gmail.com>
2021-05-29 13:30:36 +0300

    test

--- sort1.py
+++ sort1.py
@@ -1,5 +1,7 @@
 from os import listdir
 from os.path import isfile, join, splitext
+import re
+from collections import Counter
 
 # main_path = 'Z:\\usb\\USB\\[_Art]'
 main_path = 'Z:\\usb\\USB\\[_Art]\\0test'
@@ -32,3 +34,47 @@ for i in range(len(files)):
 for key in filesDict.keys():
     if len(filesDict[key]) > 1:
         print(key + ": " + str(filesDict[key]))
+
+
+####################
+# matches: match[here_is_a_match]exclude[ (any-digit)]
+# ^(?:(?! \(\d\)$).)*
+
+def remove_copy_mark(file_name):
+    pattern = re.compile(r'(\d \(\d\))$|( \(\d\))$')
+    exclude_words = ['image']
+    for exclude_word in exclude_words:
+        if re.sub(pattern, '', file_name).find(exclude_word) == -1:
+            print('test1')
+            return file_name.split()[0]
+
+
+def normalize_name(file_name):
+    pattern = re.compile(r'[^a-z0-9\-_]')
+    return re.sub(pattern, '', file_name)
+
+
+def count_spaces(string):
+    counter = 0
+    for sym in string:
+        if sym == ' ':
+            counter += 1
+    return counter
+
+
+def replace_spaces_by_matches_count(string: str, matches_count: int):
+    sym_list = list(string)
+    for index, item in enumerate(sym_list):
+        if item == ' ':
+            sym_list[index] = '_'
+            matches_count -= 1
+        if matches_count == 0:
+            result = ''
+            return result.join(sym_list)
+
+
+def normalize_duplicates_name(string: str):
+    return replace_spaces_by_matches_count(string, count_spaces(string)-1)
+
+test_string = 'gu neio rw(2)4 (3)5-yv y5{@ #$%3_#*(&@u (1)'
+print(normalize_duplicates_name(test_string))
